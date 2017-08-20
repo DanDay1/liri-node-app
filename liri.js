542dc57f40ed671c0a2f3241e46325d5d41246c4
Daniel Day <danielday@Daniels-MacBook-Pro.local>
2017-08-20 13:19:03 -0400

    current update

--- liri.js
+++ liri.js
@@ -10,6 +10,17 @@ var keys = require('./keys.js');
 var twitter = require("twitter");
 var spotify = require("spotify");
 
+
+
+//spotify trial code from office hours:
+
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: 'ca299c04c542498e831715e97c41e9b9',
+  secret: '19aac6a2f3024405b2d8c51f65afbaad'
+});
+ 
 spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
@@ -18,3 +29,36 @@ spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, d
 console.log(data); 
 });
 
+//Origional Spotify code:
+
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
+ 
+// console.log(data); 
+// });
+
+//IMDB
+
+var request = require("request");
+
+// Grab the movieName which will always be the third node argument.
+var movieName = process.argv[2];
+
+// Then run a request to the OMDB API with the movie speified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+request(queryUrl, function(error, response, body) {
+
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
+
+    // Parse the body of the site and recover just the imdbRating
+    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    console.log("Release Year: " + JSON.parse(body).Year);
+  }
+});
